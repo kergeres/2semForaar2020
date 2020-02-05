1b070a1669669651c0af72604db142c98be25f3a
David <eaadpa@students.eaaa.dk>
2020-02-05 08:44:36 +0100

    in class we

--- javascriptBoilerPlates/js/js.js
+++ javascriptBoilerPlates/js/js.js
@@ -2,4 +2,38 @@
  * file: js.js
  * purpose: interactivity
  **/
+/* console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
+
+//paste your js here
+//the img name shoub be real image names
+
+var myGallery = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg"
+]
+
+for (i = 0; i < myGallery.length; i++) {
+    // note the shorthand code below
+    exhibition.innerHTML += '<img src="images/' + myGallery[i] + '" alt=from my galleries " >';
+}*/
+
 console.log('Stardate 235678.89: JavaScript from js/js.js is up and running.');
+
+
+// the image names should be real image names
+var myGallery = [
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg"
+];
+
+for (i = 0; i < myGallery.length; i++) {
+    // note the shorthand code below
+    exhibition.innerHTML += '<img src="images/' +
+        myGallery[i] +
+        '" alt="from my galleries ">';
+}
+// let the loop create the HTML for your gallery
+// let the loop create the HTML for your gallery
